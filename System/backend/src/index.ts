926fa7fb6f27a57f72a61561058a8b8aa4ab0ebc
jailsonsdsj <jailson.junior36@hotmail.com>
2026-04-22 01:58:10 -0300

    Install Express and types

--- System/backend/src/index.ts
+++ System/backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Backend running...");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
